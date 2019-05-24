6c30b4c50389265af39211884acb756b7e8328a4
Kyle Elyk <kyleelyk92@hotmail.com>
2019-05-24 11:40:34 -0500

    trying to fix the image links

--- js/sw.js
+++ js/sw.js
@@ -8,16 +8,16 @@ const itemsToCache = [
   './restaurant_info.js', 
   '../css/styles.css', 
   '../data/restaurants.json', 
-  '../img/1.jpg', 
-  '../img/2.jpg', 
-  '../img/3.jpg', 
-  '../img/4.jpg', 
-  '../img/5.jpg', 
-  '../img/6.jpg', 
-  '../img/7.jpg', 
-  '../img/8.jpg', 
-  '../img/9.jpg', 
-  '../img/10.jpg'
+  '../restaurant-app/img/1.jpg', 
+  '../restaurant-app/img/2.jpg', 
+  '../restaurant-app/img/3.jpg', 
+  '../restaurant-app/img/4.jpg', 
+  '../restaurant-app/img/5.jpg', 
+  '../restaurant-app/img/6.jpg', 
+  '../restaurant-app/img/7.jpg', 
+  '../restaurant-app/img/8.jpg', 
+  '../restaurant-app/img/9.jpg', 
+  '../restaurant-app/img/10.jpg'
 ]
 
 
